b450afd37e2df9ca754fb872873ca3445acf9d9c
Cheikh Semeta <uzikwebmaster@gmail.com>
2017-06-06 01:12:50 +0000

    Updating class

--- NetRequest/src/main/java/com/github/thunder413/netrequest/NetRequest.java
+++ NetRequest/src/main/java/com/github/thunder413/netrequest/NetRequest.java
@@ -4,6 +4,11 @@ package com.github.thunder413.netrequest;
 import android.util.Log;
 
 public class NetRequest {
+    private static final String LOG_TAG = "LOG_TAG";
+
+    public NetRequest(){
+        Log.d(LOG_TAG,"OK");
+    }
     public static void intro(){
         Log.d("NetRequet","Ok its work");
     }
