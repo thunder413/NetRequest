346a658a3f29a7586324661a6bfe16c64e27f5b7
Cheikh Semeta <uzikwebmaster@gmail.com>
2017-06-06 00:59:51 +0000

    First class

---
+++ NetRequest/src/main/java/com/github/thunder413/netrequest/NetRequest.java
@@ -0,0 +1,10 @@
+package com.github.thunder413.netrequest;
+
+
+import android.util.Log;
+
+public class NetRequest {
+    public static void intro(){
+        Log.d("NetRequet","Ok its work");
+    }
+}
