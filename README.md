bd9e240b6bca20b003c95f89e8ed049634043208
thunder413 <thunder413@live.fr>
2017-06-07 03:32:55 +0000

    add version to readme

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # NetRequest
 
 NetRequest is a simple HTTPRequest manager that make easy to deal with Json responses
- 
+
 ### Requirements
 
 NetRequest can be included in any Android application.
@@ -25,12 +25,45 @@ allprojects {
 And then simply add the following line to the `dependencies` section of your `build.gradle` file:
 
 ```groovy
-compile 'com.github.thunder413:NetRequest:version'
+compile 'com.github.thunder413:NetRequest:1.2'
 ```
+### Performing Get / Post request
+
+```java
+NetRequest netRequest = new NetRequest(context);
+// Set Request method #NetRequest.METHOD_POST | #NetRequest.METHOD_GET (Default)
+netRequest.setRequestMethod(NetRequest.METHOD_POST);
+// Bind Listener
+netRequest.setOnResponseListener(new OnNetResponse() {
+       @Override
+       public void onNetResponseCompleted(NetResponse response) {
+           // Get response as string
+           Log.d("TAG",response.toString());
+           // Get response as JsonObject
+            response.toJson();
+         }
+
+         @Override
+         public void onNetResponseError(NetError error) {
+             // Handle error
+             switch (error.getError()) {
+                    
+             }
+         }
+ });
+ // Set Request uri (Support both uri and url string)
+ netRequest.setRequestUri("http://demo.com/json");
+ // Trigger request
+ netRequest.load();
+ // You can also pass the uri directly to load method
+ netRequest.load("http://demo.com/json");
+```
+
+
 
 ## Author
 
-* **Thunder413** - *Initial work* - [Thunder413](https://github.com/thunder413)
+* **Thunder413** (https://github.com/thunder413)
 
 
 
@@ -42,6 +75,5 @@ This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md
 
 * HTTPRequest from kevinsawicki
 * Gson Google
-
 
 
--- app/src/main/java/com/github/thunder413/netrequestsample/MainActivity.java
+++ app/src/main/java/com/github/thunder413/netrequestsample/MainActivity.java
@@ -6,9 +6,15 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.github.thunder413.netrequest.NetError;
+import com.github.thunder413.netrequest.NetRequest;
+import com.github.thunder413.netrequest.NetResponse;
+import com.github.thunder413.netrequest.OnNetResponse;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +33,34 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        NetRequest netRequest = new NetRequest(this);
+        // Set Request method #NetRequest.METHOD_POST | #NetRequest.METHOD_GET (Default)
+        netRequest.setRequestMethod(NetRequest.METHOD_POST);
+        // Bind Listener
+        netRequest.setOnResponseListener(new OnNetResponse() {
+            @Override
+            public void onNetResponseCompleted(NetResponse response) {
+                // Get response as string
+                Log.d("TAG",response.toString());
+                // Get response as JsonObject
+                response.toJson();
+            }
+
+            @Override
+            public void onNetResponseError(NetError error) {
+                // Handle error
+                switch (error.getError()) {
+
+                }
+            }
+        });
+        // Set Request
+        netRequest.setRequestUri("http://demo.com/json");
+        // Trigger request
+        netRequest.load();
+        // You can also pass the uri directly to load method
+        netRequest.load("http://demo.com/json");
+
     }
 
     @Override
